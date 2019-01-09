703c227d7da949afb3478221180aa5124a4f9702
Victoria B <viktorija.bourassa@gmail.com>
2019-01-09 22:23:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,7 +31,7 @@ end
 
 
 def valid_move?(board, position)
- position.between?(0,8) && !position_taken?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
  def turn(board)
